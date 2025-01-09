a7c7189f7244c438699f761d7883406ec49530c8
Pedro Augusto Gomes Minare <pedrominare94@gmail.com>
2025-01-09 09:02:40 -0300

    concatenar dfs

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 # monta os DFs
+import pandas as pd
+
 from src.data.build_df import BuildDF
+from src.utils.extract_data import obtem_dados_municipio
 from src.utils.load_file import get_sheet_names
 
 # arquivos XLSX com datasets
 xlsx_casos_dengue = 'G:\\Meu Drive\\MBA-DATA-SCIENCE\\TCC\\dados\\GOIANIA_2010_dengue-casos-notificados.xlsx'
 xlsx_municipios = 'G:\\Meu Drive\\MBA-DATA-SCIENCE\\TCC\\dados\\DADOS-INDICADORES-SOCIOECONOMICOS\\DATA-SET-2010.xlsx'
+municipio_interesse = "Goiânia"
 
 # obtem nomes das planilhas do xlsx
 sheet_names_casos_dengue = get_sheet_names(xlsx_casos_dengue)
@@ -19,8 +23,18 @@ for sheet_municio in sheet_names_municipios:
         sheet_name=sheet_municio,
         skiprows=1
     )
-    lista_dfs_municipios.append(df_municipio)
+    df_municipio.df["indice_socioeconomico"] = sheet_municio  # adiciona o nome da planilha em todas as observacoes
+    df_municipio.get_variables()
+    dados_municipio = obtem_dados_municipio(
+        df=df_municipio.df,
+        municipio=municipio_interesse,
+        colunas=df_municipio.variables
+    )
+    lista_dfs_municipios.append(dados_municipio)
     del df_municipio
+
+# unir todos os dataframes de municipios com suas respectivas colunas
+df_concatenado = pd.concat(lista_dfs_municipios, ignore_index=True)
 
 for sheet_municio in sheet_names_casos_dengue:
     df_dengue = BuildDF(
---
+++ src/utils/extract_data.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from pandas import DataFrame
+import unicodedata
+
+from src.data.build_df import BuildDF
+
+
+def normalizar_string(text):
+    try:
+        return ''.join(c for c in unicodedata.normalize('NFD', text) if unicodedata.category(c) != 'Mn').lower()
+    except Exception as error:
+        raise Exception(f"Erro ao normalizar a string {text}! {error}")
+
+
+def obtem_dados_municipio(df: DataFrame, municipio: str, colunas: list):
+    coluna_municipio = None
+    municipio_normalizado = ''
+    nome_padronizado_coluna = 'municipio'
+
+    for coluna in colunas:
+        try:
+            municipio_normalizado = normalizar_string(municipio)
+            df_municipio_normalizado = df[coluna].apply(normalizar_string)
+        except Exception as error:
+            raise Exception(f"Erro ao normalizar municipio e coluna de municipio do dataframe! {error}")
+
+        if municipio_normalizado in df_municipio_normalizado.values:
+            coluna_municipio = nome_padronizado_coluna  # padronizacao dos nomes de colunas de municipio.
+            df = df.rename(columns={coluna: coluna_municipio})
+            df[coluna_municipio] = df[coluna_municipio].apply(normalizar_string)
+            break
+
+    if coluna_municipio:
+        linha_municipio = df[df[coluna_municipio] == municipio_normalizado]
+    else:
+        raise ModuleNotFoundError(f"Municipio nao encontrado no df!")
+
+    return linha_municipio
